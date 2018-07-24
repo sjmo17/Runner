f86c1feeb39fae80f717dca112aa151114af15b6
Steven Mo <mostevenj@gmail.com>
2018-07-24 09:59:32 -0700

    Initial Commit

---
+++ Runner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Runner
+//
+//  Created by Steven Mo on 7/24/18.
+//  Copyright © 2018 Steven Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
