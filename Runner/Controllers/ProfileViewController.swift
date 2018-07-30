ba2285fd52c839baa60e4e253678a61dcde52eb0
Steven Mo <mostevenj@gmail.com>
2018-07-30 16:41:01 -0700

    Use username in Profile

--- Runner/Controllers/ProfileViewController.swift
+++ Runner/Controllers/ProfileViewController.swift
@@ -12,10 +12,22 @@ import FirebaseDatabase
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var profileUsernameLabel: UILabel!
+    @IBOutlet weak var totalMilesLabel: UILabel!
+    @IBOutlet weak var totalRunsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        guard let firUser = Auth.auth().currentUser else { return }
+        let ref = Database.database().reference().child("users").child(firUser.uid).child("username")
+        ref.observeSingleEvent(of: .value) { (snapshot) in
+            if let value = snapshot.value as? String {
+                self.profileUsernameLabel.text = value
+            }
+        }
+        
     }
     
     
